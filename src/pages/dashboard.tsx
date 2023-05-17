496ff84f7735f2ee52eb37945efc7327196aac68
Bruna <bruna.varela@shoppersupply.com.br>
2023-05-17 15:37:38 -0300

    fix dashboard layout responsive

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -32,7 +32,7 @@ const Home: NextPage = () => {
       <main className="bg-branco-secundario">
         <Menu />
         <Modal />
-        <div>
+        <div className="flex justify-end">
           <ModalAprovacaoErros />
           {isFirstVisit && <WellcomeModal setFirstVitit={setFirstVitit} />}
           {componentsPageDash[0]}
