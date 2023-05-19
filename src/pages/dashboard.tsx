a5e7470e3009180457941798353d2a2905589c1b
Bruna <bruna.varela@shoppersupply.com.br>
2023-05-19 12:16:35 -0300

    fix responsive  modalApravacaoErros

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -32,11 +32,9 @@ const Home: NextPage = () => {
       <main className="bg-branco-secundario">
         <Menu />
         <Modal />
-        <div className="flex justify-end">
-          <ModalAprovacaoErros />
-          {isFirstVisit && <WellcomeModal setFirstVitit={setFirstVitit} />}
-          {componentsPageDash[0]}
-        </div>
+        {isFirstVisit && <WellcomeModal setFirstVitit={setFirstVitit} />}
+        {componentsPageDash[0]}
+        <ModalAprovacaoErros />
       </main>
     </>
   );
