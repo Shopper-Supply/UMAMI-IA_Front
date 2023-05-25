ea297d0e87f95270e0119cc6f170e1a40eaefd23
Alex Lanção <lancao.af@gmail.com>
2023-05-25 10:49:05 -0300

    fix: modal menu scrool

--- src/components/modalSku/index.tsx
+++ src/components/modalSku/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import ListaModalSku from "../listaModalSku";
 
 const ModalSku = () => {
+  const listalengt = 5;
   return (
     <div className="absolute z-50 top-0 w-screen h-screen flex justify-center items-center backdrop-blur-sm bg-black bg-opacity-20">
       <div className="bg-white w-[60%] h-[80%] rounded-md relative ml-10 ">
@@ -24,21 +25,35 @@ const ModalSku = () => {
             </p>
           </div>
         </div>
-        <div className="flex justify-center mt-10">
-          <button className="bg-white border-2 border-roxo-primario font-bold focus:bg-roxo-primario focus:text-white text-[1.2rem] text-roxo-primario rounded-full p-5 m-2">
-            {" "}
-            SKU: 3275878122509 - 3x
-          </button>
-          <button className="bg-white border-2 border-roxo-primario font-bold focus:bg-roxo-primario focus:text-white text-[1.2rem] text-roxo-primario rounded-full p-5 m-2">
-            {" "}
-            SKU: 3275878122509 - 3x
-          </button>
-          <button className="bg-white border-2 border-roxo-primario font-bold focus:bg-roxo-primario focus:text-white text-[1.2rem] text-roxo-primario rounded-full p-5 m-2">
-            {" "}
-            SKU: 3275878122509 - 3x
-          </button>
+        <div className="w-[100%] overflow-x-auto">
+          <div
+            className={`flex gap-3 mt-10 ${
+              listalengt < 7 ? "justify-center" : "ml-5"
+            }`}
+          >
+            <button className="bg-white border-2 h-[4rem] border-roxo-primario font-bold focus:bg-roxo-primario focus:text-white text-[1.2rem] text-roxo-primario rounded-full px-7 leading-5">
+              {" "}
+              2X - SKU: <br /> 3275878122509
+            </button>
+            <button className="bg-white border-2 h-[4rem] border-roxo-primario font-bold focus:bg-roxo-primario focus:text-white text-[1.2rem] text-roxo-primario rounded-full px-7 leading-5">
+              {" "}
+              2X - SKU: <br /> 3275878122509
+            </button>
+            <button className="bg-white border-2 h-[4rem] border-roxo-primario font-bold focus:bg-roxo-primario focus:text-white text-[1.2rem] text-roxo-primario rounded-full px-7 leading-5">
+              {" "}
+              2X - SKU: <br /> 3275878122509
+            </button>
+            <button className="bg-white border-2 h-[4rem] border-roxo-primario font-bold focus:bg-roxo-primario focus:text-white text-[1.2rem] text-roxo-primario rounded-full px-7 leading-5">
+              {" "}
+              2X - SKU: <br /> 3275878122509
+            </button>
+            <button className="bg-white border-2 h-[4rem] border-roxo-primario font-bold focus:bg-roxo-primario focus:text-white text-[1.2rem] text-roxo-primario rounded-full px-7 leading-5">
+              {" "}
+              2X - SKU: <br /> 3275878122509
+            </button>
+          </div>
         </div>
-        <div className="flex justify-center overflow-y-scroll h-[33.5rem]">
+        <div className="flex justify-center overflow-y-scroll h-[30rem]">
           <div className="flex flex-col items-start w-[50vw] mt-5 gap-3">
             <ListaModalSku />
             <ListaModalSku />
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -37,7 +37,7 @@ const Home: NextPage = () => {
         <Menu />
         <Modal />
         {loadingScreen && <LoadingScreen />}
-        <ModalSku />
+        {/* <ModalSku /> */}
         <div className="flex justify-end">
           <ModalAprovacaoErros />
           {isFirstVisit && <WellcomeModal setFirstVitit={setFirstVitit} />}
