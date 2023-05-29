16e661d96966a22297f0d4dbca3d76340e698c28
Alex Lanção <lancao.af@gmail.com>
2023-05-29 09:44:52 -0300

    feat: conection w back-end

--- src/services/patch.ts
+++ src/services/patch.ts
@@ -1,1 +1,19 @@
-import api from "./"
+import { IRepitedSku, ISheet } from "@/interfaces/sheet";
+import api from ".";
+
+export function updateSku(
+  token: string | undefined,
+  sku_id: string,
+  body: IRepitedSku
+) {
+  // Apagar um Sku
+  const sku = api
+    .patch(`/sku/${sku_id}`, {
+      headers: {
+        Authorization: "Token " + token,
+      },
+    })
+    .then((res) => res)
+    .catch((err) => console.error(err));
+  return sku;
+}
