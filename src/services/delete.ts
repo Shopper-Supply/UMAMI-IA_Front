5df0fa2fb3ce87dbf71db7ba0aabb967e3abb4da
Alex Lanção <lancao.af@gmail.com>
2023-05-31 10:20:45 -0300

    fix: conflict services delete

--- src/services/delete.ts
+++ src/services/delete.ts
@@ -1,28 +1,26 @@
-<<<<<<< HEAD
 import api from ".";
+import { IUserDetail } from "@/interfaces/people";
 
 export function deleteSku(token: string | undefined, sku_id: string) {
   // Apagar um Sku
   const sku = api
     .delete(`/sku/${sku_id}`, {
-=======
-import { IUserDetail } from "@/interfaces/people";
-import api from ".";
-
-export function deleteUser(token: string | undefined, body: IUserDetail) {
-  body.is_active = false;
-  const response = api
-    .patch(`suspender/usuario/${body.id}/`, body, {
->>>>>>> b973aa2444661d916f25fa2cf8247ac5f003ccfa
       headers: {
         Authorization: "Token " + token,
       },
     })
-<<<<<<< HEAD
     .then((res) => res)
     .catch((err) => console.error(err));
   return sku;
-=======
+}
+export function deleteUser(token: string | undefined, body: IUserDetail) {
+  body.is_active = false;
+  const response = api
+    .patch(`suspender/usuario/${body.id}/`, body, {
+      headers: {
+        Authorization: "Token " + token,
+      },
+    })
     .then((res) => res.data)
     .catch((err) => console.error(err));
 
@@ -41,5 +39,4 @@ export function activeUser(token: string | undefined, body: IUserDetail) {
     .catch((err) => console.error(err));
 
   return response;
->>>>>>> b973aa2444661d916f25fa2cf8247ac5f003ccfa
 }
