a5a5041307803d044843d7e2b6c71b22099a0efd
Alex Lanção <lancao.af@gmail.com>
2023-06-03 20:40:00 -0300

    feat: delete all files

--- src/interfaces/bagpattern.ts
+++
@@ -1,7 +0,0 @@
-export interface IBag {
-    iva?: number;
-    width?: number;
-    height?: number;
-    weight?: number;
-    length?: number;
-}
--- src/utils/formatData.ts
+++
@@ -1,8 +0,0 @@
-export function extractDate(dateTimeString: string): string {
-  const dateTime = new Date(dateTimeString);
-  const day = String(dateTime.getDate()).padStart(2, "0");
-  const month = String(dateTime.getMonth() + 1).padStart(2, "0");
-  const year = dateTime.getFullYear();
-  const formattedDate = `${day}/${month}/${year}`;
-  return formattedDate;
-}
