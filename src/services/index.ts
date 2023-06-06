cc8418c8f30ccf1fdcdb3a2395799e5328fb4184
Alex Lanção <93793324+lancao2@users.noreply.github.com>
2023-06-06 15:10:53 -0300

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
-  // baseURL: "https://robo-qualidade.azurewebsites.net/api",
+//   baseURL: "http://localhost:8000/api",
+  baseURL: "https://robo-qualidade.azurewebsites.net/api",
 });
 
 export default api;
